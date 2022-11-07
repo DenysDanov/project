20a85cd4dab79620108907858ea0a08538ea31ba
Danov Denys <denysdanov@gmail.com>
2022-11-07 14:26:40 +0200

    asd

--- main/models.py
+++ main/models.py
@@ -15,15 +15,45 @@ class CarProducer(models.Model):
 
 
 class Auto(models.Model):
+    
+    WHEEL_DRIVE_CHOICES = [
+        ('Повний', '1'),
+        ('Передній', '2'),
+        ('Задній', '3'),
+    ]
+    FUEL_CHOICES = [
+        ('Бензин', 0),
+        ('Електро', 1),
+        ('Дизель', 2),
+    ]
+    BODY_CHOICES = [
+        ('Седан', 0),
+        ('Кросовер', 1),
+        ('Мінівен', 2),
+        ('Мікровен', 3),
+        ('Хетчбек', 4),
+        ('Універсал', 5),
+        ('Купе', 6),
+        ('Кабріолет', 7),
+        ('Пікап', 8),
+        ('Ліфтбек', 9),
+        ('Фастбек', 10),
+        ('Лімузин', 11),
+        ('Родстер', 12),
+    ]
+    vin = models.CharField('VIN код', max_length=64, primary_key=True)
     model = models.CharField("Модель", max_length=255)
     producer = models.ForeignKey(
         verbose_name = "Виробник", 
         to = CarProducer, 
         on_delete = models.CASCADE)
     year_of_production = models.IntegerField("Рік випуску")
+    engine_volume = models.FloatField('Об\'єм двигуна')
+    wheel_drive = models.CharField('Привід', max_length=255, choices=WHEEL_DRIVE_CHOICES)
+    fuel = models.CharField('Тип палива', max_length=255, choices=FUEL_CHOICES)
 
     def __str__(self) -> str:
-        return f'{self.producer} {self.model} {self.year_of_production}р.'
+        return f'{self.vin} | {self.producer} {self.model} {self.year_of_production}р.'
 
     class Meta:
         verbose_name = "Автомобіль"
@@ -31,13 +61,14 @@ class Auto(models.Model):
 
 
 class Part(models.Model):
+
     name = models.CharField('Назва', max_length=255)
     belongs_to = models.ForeignKey(
             verbose_name = "Автомобіль", 
             to = Auto, 
             on_delete = models.DO_NOTHING
         )
-    amount = models.IntegerField('Кількість')
+    amount = models.IntegerField('Кількість', blank=True)
     
     def __str__(self) -> str:
         return f'{self.name} від {self.belongs_to.__str__()}'
