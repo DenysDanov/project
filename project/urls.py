ea74a76b0ae03c9e470265df29c1fb4d24ff3a82
DenysDanov <denisdanov18@gmail.com>
2022-11-08 14:54:59 +0200

    upd

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from main.admin import admin_site
 
 urlpatterns = [
     path('admin/', admin_site.urls),
     path('', include('main.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler500 = 'main.views.handler500'
